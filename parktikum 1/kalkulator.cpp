3294256cb41f3adc34985bb960ee6b4aedfe67dd
fahreza370 <101689358+fahreza370@users.noreply.github.com>
2022-03-19 16:43:20 +0700

    Update kalkulator.cpp

--- parktikum 1/kalkulator.cpp
+++ parktikum 1/kalkulator.cpp
@@ -11,11 +11,11 @@ int main(){
 	string operasi;
 	
 	cout<<"PILIH OPERATOR ARITMATIKA"<<endl;
-	cout<<"1. Penjumlahan	|"<<endl;
-	cout<<"2. Pengurangan	|"<<endl;
-	cout<<"3. Perkalian	|"<<endl;
-	cout<<"4. Pembagian	|"<<endl;
-	cout<<"5. Modulus	|"<<endl;
+	cout<<"|1. Penjumlahan		|"<<endl;
+	cout<<"|2. Pengurangan		|"<<endl;
+	cout<<"|3. Perkalian		|"<<endl;
+	cout<<"|4. Pembagian		|"<<endl;
+	cout<<"|5. Modulus		|"<<endl;
 	cout<<endl;
 	
 	cout<<"Masukan Pilihan : ";
